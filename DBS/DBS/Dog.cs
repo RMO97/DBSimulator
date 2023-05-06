47025d0ae449290f8b296598845d62de12c570fe
RMO97 <belmont2097@gmail.com>
2023-05-06 16:08:09 -0600

    First Steps
    I created the project and the class I am going to use.

---
+++ DBS/DBS/Dog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBS
+{
+    internal class Dog
+    {
+        private String name { get; set; }
+        private String race { get; set; }
+        private int id { get; set; }
+
+        public Dog(string name, string race, int id)
+        {
+            this.name = name;
+            this.race = race;
+            this.id = id;
+        }
+
+
+    }
+}

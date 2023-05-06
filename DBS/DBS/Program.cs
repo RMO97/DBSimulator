504af46829c0b02bd3d917a6913827c1779b614c
RMO97 <belmont2097@gmail.com>
2023-05-06 16:50:15 -0600

    Menu
    Menu creation

--- DBS/DBS/Program.cs
+++ DBS/DBS/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+using System.ComponentModel.Design;
+
+namespace DBS
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        public static void Menu()
+        {
+            Console.WriteLine("Welcome to Happy Dog House");
+            Console.WriteLine("Please select one of our menu opcions");
+            Console.WriteLine($"\n 1. Register a dog \n 2. See list of dogs \n 3. Delete a dog \n 4. Update a dog \n 5. Exit from menu ");
+
+        }
+    }
+
+}
